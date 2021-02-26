93759ab73ab4520a4f1640d3d2a61260e014bf3d
Carolyn Cui <carolyn.cui@gmail.com>
2021-02-26 13:25:27 -0800

    updates

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,19 +7,62 @@ public class PlayerControl : MonoBehaviour
     private float speed = 5.0f;
     private float horizontalInput;
     private float forwardInput;
+    private float xRange = 7;
+    private float zRange = 7;
+
+    public GameObject foodProjectile;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foodProjectile.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -90);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, 90);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch a projectile from the player
+            Instantiate(foodProjectile, transform.position, foodProjectile.transform.rotation);
+        }
+
     }
 }
---
+++ Assets/Scripts/SpawnControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float startDelay = 5;
+    private float spawnInterval = 10f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnCustomer", startDelay, spawnInterval);
+    }
+
+    public GameObject[] customers;
+
+    private float spawnRangeX = -1;
+    private float spawnPosZ = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnCustomer()
+    {
+        {
+            int customerIndex = Random.Range(0, customers.Length-1);
+
+            Instantiate(customers[customerIndex], new Vector3(spawnRangeX, 0, spawnPosZ), customers[customerIndex].transform.rotation);
+        }
+    }
+}
